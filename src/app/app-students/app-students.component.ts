9821730ea3355dfa611004b2f95ae1676e328f41
amador toledo <amadortoledo5@gmail.com>
2024-03-20 02:56:17 -0300

    primer commit

---
+++ src/app/app-students/app-students.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-students',
+  templateUrl: './app-students.component.html',
+  styleUrl: './app-students.component.scss'
+})
+export class AppStudentsComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { AppStudentsComponent } from './app-students/app-students.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    AppStudentsComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
